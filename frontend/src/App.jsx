31ed0eb1138b1020b07a7f64f7dc908be7b1db02
iNoah-Guy <nprothschild@gmail.com>
2025-04-06 18:58:53 +0200

    Added Landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Register from "./pages/Register";
 import Pantry from "./pages/Pantry";
 import RecipeDetail from "./pages/RecipeDetail";
 import Recipes from "./pages/Recipes";
+import Home from "./pages/Home";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function Logout() {
@@ -18,11 +19,13 @@ function RegisterAndLogout() {
 }
 
 function App() {
+  const { isAuthentcated } = useAuth();
+
   return (
     <BrowserRouter>
       <Routes>
         <Route
-          path="/"
+          path="/pantry"
           element={
             <ProtectedRoute>
               <Pantry />
@@ -45,6 +48,7 @@ function App() {
             </ProtectedRoute>
           }
         />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<RegisterAndLogout />} />
       </Routes>
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div>Welcome to Chefbook!</div>;
+} 
+
+export default Home;
